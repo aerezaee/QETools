4a73e05339ecde4049d4bb380c8c1123ffda1169
unknown <aerezaee@live.com>
2019-04-21 09:57:45 +0430

    Using Config.ini

--- Run.py
+++ Run.py
@@ -28,6 +28,9 @@ from classes.espressoParser import *;
 from classes.nebUI import *;
 from classes.pdos import *;
 from classes.cutoffUI import *;
+from classes import config
+import json
+from configparser import ConfigParser
 
 uiMainWindow, QtBaseClass = uic.loadUiType("ui.ui")
 
@@ -47,22 +50,43 @@ class App(QtWidgets.QMainWindow, uiMainWindow):
         self.nebBtn.clicked.connect(self.nebBtnHandler);
         self.pdosBtn.clicked.connect(self.pdosBtnHandler);
         self.cutoffBtn.clicked.connect(self.cutoffBtnHandler);
+        self.config = ConfigParser();
+        self.config.read("config.ini");
         # Define Files
         self.fileLocation = ""
-        self.directory = os.path.dirname(os.path.realpath(__file__))
-        self.yRange = [-1, 1]
-        self.xTicks = ["G", "Y", "X", "Z", "G"]
-        self.xTicksLocations = [];
+        dir = self.config.get("run","directory");
+        if (dir == ""):
+            self.directory = os.path.dirname(os.path.realpath(__file__))
+            self.saveConfig(section = "run",key = "directory",value = self.directory);
+        else:
+            self.directory = dir;
+        #Get values from Config.ini files and put them in the input text boxes
+        self.yRange = eval(self.config.get("run","yRange"));
+        self.xTicks = self.config.get("run","xTicks").replace("\"","").replace("\'","").replace(" ","");
+        self.xTicksLocations = self.config.get("run","xTicksLocations");
+        self.yRangeInput.setText(str(self.yRange).replace("[","").replace("]",""));#yInputText
+        self.directoryLabel.setText(str(self.directory));
+        self.xRangeInput.setText(str(self.xTicks).replace("[","").replace("]","").replace("\"",""));
+        self.xTicksLocationInput.setText(str(self.xTicksLocations).replace("[","").replace("]",""));
+    
         self.xmlFile = "";
         self.inputs = "";
         self.outputs = "";
         self.fileOpened = False
 
+    def saveConfig(self,configFile = "config.ini",section = "run",key = "directory", value = ""):
+        if ((key == "directory") and (value == "")):
+            value = self.directory;
+        self.config.set(section,key,str(value));
+        file = open(configFile,"w+");
+        self.config.write(file);
+        file.close()
     def oepnXMLBtnHandler(self):
 
         self.xmlFile = self.fileChooser(type="open")
         if (str(self.xmlFile).lower().endswith(".xml")):
             self.directory = os.path.dirname(os.path.abspath(self.xmlFile))
+            self.saveConfig();
             self.directoryLabel.setText(str(self.directory))
             self.plotBandBtn.setEnabled(True)
             self.saveBtn.setEnabled(True)
@@ -73,16 +97,19 @@ class App(QtWidgets.QMainWindow, uiMainWindow):
 
     def plotBandBtnHandler(self):
         try:
-            self.yRange = eval(self.yRangeInput.text())
+            #self.yRange = eval(self.yRangeInput.text())
+            self.yRange = [float(i) for i in self.yRangeInput.text().split(",")];
             self.outputs.yLim = self.yRange
         except Exception as e:
             self.errorMessage("Wrong Value", "Please enter the range as ymin,ymax like -1,1");
+
         try:
-            self.xTicks = self.xRangeInput.text().split(",");
+            self.xTicks = self.xRangeInput.text().replace(" ","").replace("\"","").split(",");
         except Exception as e:
             self.errorMessage("Wrong Value", "Please enter the range as G,X,Y,Z");
         try:
             xTicksLocations = eval(self.xTicksLocationInput.text());
+            print ("xTickslocations:",xTicksLocations,self.xTicks,self.xTicksLocationInput.text())
             if len(xTicksLocations) == len(self.xTicks) and np.max(xTicksLocations) <= self.outputs.nKPoints:
                 self.xTicksLocations = xTicksLocations;
             else:
@@ -90,15 +117,17 @@ class App(QtWidgets.QMainWindow, uiMainWindow):
 
         except Exception as e:
             self.xTicksLocations = np.linspace(0, self.outputs.nKPoints - 1, num=len(self.xTicks));
-            print(str(e))
             self.errorMessage("Value Problem",
                               "Please Enter the ticks locations in proper format and compatible with tick labels");
-        print(self.xTicksLocations)
+
+
         self.outputs.tickLabels = self.xTicks;
         self.outputs.tickLocations = self.xTicksLocations;
-        # print (self.xTicks, self.xTicksLocations);
-        self.outputs.plotBand(usingAntonate=self.showBandCB.isChecked(
-        ), usingLines=self.showLinesCB.isChecked(), usingXTicks=self.xTicksCB.isChecked())
+        
+        self.saveConfig(key = "xTicks",value = self.xTicks);
+        self.saveConfig(key = "xTickLocations",value = self.xTicksLocations);
+        self.saveConfig(key = "yRange",value = self.yRange);
+        self.outputs.plotBand(usingAntonate=self.showBandCB.isChecked(), usingLines=self.showLinesCB.isChecked(), usingXTicks=self.xTicksCB.isChecked())
 
     def saveBtnHandler(self):
         fileName = self.fileChooser(type="save")
@@ -148,10 +177,10 @@ class App(QtWidgets.QMainWindow, uiMainWindow):
         fileName = ""
         if type == "open":
             fileName, _ = QtWidgets.QFileDialog.getOpenFileName(
-                self, "QFileDialog.getOpenFileName()", "", "All Files (*);;XML Files (*.xml)")
+                self, "QFileDialog.getOpenFileName()", self.directory, "All Files (*);;XML Files (*.xml)")
         if type == "save":
             fileName, _ = QtWidgets.QFileDialog.getSaveFileName(
-                self, "QFileDialog.getSaveFileName()", "", "All Files (*);;Text Files (*.txt)")
+                self, "QFileDialog.getSaveFileName()", self.directory, "All Files (*);;Text Files (*.txt)")
         if type == "directory":
             fileName = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         print(fileName)
@@ -162,9 +191,14 @@ class App(QtWidgets.QMainWindow, uiMainWindow):
         self.outputs = outputResults(
             fileName = self.xmlFile, path=self.directory, occu=float(self.occupationSpin.text()))
         self.outputText.setPlainText("")
-        self.outputText.setPlainText(str(self.outputs))
-        ticks = np.linspace(0, self.outputs.nKPoints - 1, num=len(self.xTicks), dtype=int).tolist();
-        self.xTicksLocationInput.setText(str(ticks).replace("[", "").replace("]", ""));
+        try:
+            self.outputText.setPlainText(str(self.outputs))
+        except Exception as e:
+            self.outputText.setPlainText(str(e));
+
+        #ticks = np.linspace(0, self.outputs.nKPoints - 1, num=len(self.xTicks), dtype=int).tolist();
+        #self.xTicksLocationInput.setText(str(ticks).replace("[", "").replace("]", ""));
+
 
 
 if __name__ == "__main__":
--- classes/config.py
+++ classes/config.py
@@ -1,17 +1,36 @@
+
 pdosColor = {
+
     1:"orange",
+
     2:"limegreen",
+
     3:"cornflowerblue",
+
     4:"tomato",
+
     5:"gold",
+
     6: "red",
+
     7: "blue",
+
     8:"green",
+
     9: "orange",
+
     "all":"gray"
+
 }
+
 pdosXLim = {
+
     "xmin":-10,
+
     "xmax":10
+
 }
+
 pdosFermi = None;
+
+directory = "Dir"
